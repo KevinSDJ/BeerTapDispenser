6ffeaa7bc23d28b098867fa6bd1653364759af74
kevin <sebadjkevin@gmail.com>
2022-11-12 02:01:53 -0300

    project created files

---
+++ src/main/java/com/api/Beerdispenser/BeerdispenserApplication.java
@@ -0,0 +1,13 @@
+package com.api.Beerdispenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerdispenserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerdispenserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/Beerdispenser/BeerdispenserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Beerdispenser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerdispenserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
