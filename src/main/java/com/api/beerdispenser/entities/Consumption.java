2172163273b0328986471edd53e968a4fa326f71
kevin De jesus <sebadjkevin@gmail.com>
2022-11-18 16:22:41 -0300

    lombok dependecy add

--- src/main/java/com/api/beerdispenser/entities/Consumption.java
+++ src/main/java/com/api/beerdispenser/entities/Consumption.java
@@ -1,7 +1,6 @@
 package com.api.beerdispenser.entities;
 
 import java.util.Date;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,10 +8,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
+@Data 
+@AllArgsConstructor
+@NoArgsConstructor
 public class Consumption {
-    
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long _id;
@@ -23,32 +28,5 @@ public class Consumption {
     @ManyToOne()
     @JoinColumn(name="dispenser_id")
     private Dispenser dispenser;
-    
-    public Consumption(Date open_at) {
-        this.open_at = open_at;
-    }
-    public Long get_id() {
-        return _id;
-    }
-    public void set_id(Long _id) {
-        this._id = _id;
-    }
-    public Date getOpen_at() {
-        return open_at;
-    }
-    public void setOpen_at(Date open_at) {
-        this.open_at = open_at;
-    }
-    public Date getClose_at() {
-        return close_at;
-    }
-    public void setClose_at(Date close_at) {
-        this.close_at = close_at;
-    }
-    @Override
-    public String toString() {
-        return "Consumption [_id=" + _id + ", open_at=" + open_at + ", close_at=" + close_at + "]";
-    }
-    
-    
+
 }
